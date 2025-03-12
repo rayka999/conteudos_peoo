1c0d3a9d9489322b65e886193c6641cdb5e6fc4f
rayka999 <raykalgarcia07@gmail.com>
2025-03-12 17:41:15 -0300

    ..

---
+++ testes_unitarios/testes_com_JS/soma.test.js
@@ -0,0 +1,10 @@
+// soma.test.js
+const soma = require('./soma');
+
+test('soma de 2 e 3 deve ser 5', () => {
+  expect(soma(2, 3)).toBe(5);
+});
+
+test('soma de números negativos', () => {
+  expect(soma(-1, -1)).toBe(-2);
+});
